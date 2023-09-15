89f8fd541e32aae3ec6f96d998baa849acda74d3
Ben Nguyen <144606837+BenNguyen1203@users.noreply.github.com>
2023-09-15 14:36:58 -0500

    Update Module 1 Coding.sql

--- Module 1 Coding.sql
+++ Module 1 Coding.sql
@@ -1,7 +1,13 @@
-﻿
+﻿--Ben Nguyen
+
+--Q1) Retrieve all the records from the Employees table.
+
 SELECT  * 
 FROM    Employees
 --------------------------------------------------------
+        
+--Q2) Retrieve the FirstName, LastName, Birthdate, Address, City, and State from
+-- the Employees table.
 
 SELECT  FirstName, 
         LastName, 
@@ -11,6 +17,9 @@ SELECT  FirstName,
         State 
 FROM    Employees
 --------------------------------------------------------
+
+--Q3) Retrieve all the columns from the Tracks table, but only return 20 rows.
+
 Select  * 
 From    Tracks
 Limit   20
